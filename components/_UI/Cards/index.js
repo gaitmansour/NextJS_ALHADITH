97485eb386e02cc83af5ec032b5af5d005f9b40d
m-benrhabra <meriem.benrhabra1@gmail.com>
2022-01-10 18:12:05 +0100

    styles ressoureces

--- components/_UI/Cards/index.js
+++ components/_UI/Cards/index.js
@@ -1,13 +1,16 @@
-import styles from './Cards.module.css';
+import styles from './Cards.module.css'
 
 const Cards = (props) => {
-    return (
-        <div className={`my-4 overflow-hidden p-4 rounded-3 shadow-card ${props?.className ? props.className : ""}`}
-             style={props.style}>
-
-            {props?.children}
-        </div>
-    );
+  return (
+    <div
+      className={`my-4 overflow-hidden p-4 rounded-3 shadow-card ${
+        props?.className ? props.className : ''
+      }`}
+      style={props.style}
+    >
+      {props?.children}
+    </div>
+  )
 }
 /*
                  backgroundColor: '#FFFFFF',
