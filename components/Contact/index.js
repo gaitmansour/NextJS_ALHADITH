92a386cad8c519b8dec3325706db4d15659d6a77
ghizlane <g.aitmansour@dialy.net>
2022-01-17 10:40:05 +0100

    Fix bugs

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -9,7 +9,7 @@ const Contact = (props) => {
     return (
         <div
             className={`${styles.Contact} Contact bg-success-light d-flex align-items-center py-2 px-5 justify-content-around flex-wrap ${className}`}>
-            <h2 className="text-white">{title}</h2>
+            <h2 className="text-white">{"تواصلوا معنا عبر"}</h2>
             <div className={`${styles.devider} devider bg-white`}/>
             <SMLinks row={true} className={`${styles.SMLinks} flex-row-reverse`}/>
         </div>
