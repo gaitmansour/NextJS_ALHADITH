a76abf192092275e29b057c0ea51e651a68251dc
ghizlane <g.aitmansour@dialy.net>
2022-01-20 14:10:04 +0100

    Add articles By tag

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,74 +1,76 @@
 //export const base_url = `https://back.alhadith.dialtechnologies.net`
-//const {BASE_URL_CMS, BASE_URL_ES} = process.env;
-export const base_url = `https://backend.7adith.ma:8000`
-export const api_url = `${base_url}/en/jsonapi/node`
+export const base_url = `https://backend.7adith.ma:8000`;
+const {BASE_URL_CMS} = process.env;
+export const api_url = `${base_url}/en/jsonapi/node`;
 //export const api_url = `http://backend.7adith.ma:8000/en/jsonapi/node`
-export const api_search = `https://apisearch.7adith.ma:8000`
-export const api_newsletter = `https://aqueous-basin-44895.herokuapp.com/https://us5.api.mailchimp.com/3.0/lists/75912977ff/members`
+export const api_search = `https://apisearch.7adith.ma:8000`;
+export const api_newsletter = `https://apisearch.7adith.ma:8000/api/addmembre`;
 // export const base_url = `https://api.pray.zone/`
 
 export const getResourcesData = (langcode, value = 'موارد') =>
-  `${api_url}/section?fields[node--section]=title,body,field_icone,field_lien&filter[langcode]=${langcode}&filter[alqsm][condition][path]=field_alqsm.name&filter[alqsm][condition][value]=${value}&include=field_icone`
+    `${api_url}/section?fields[node--section]=title,body,field_icone,field_lien&filter[langcode]=${langcode}&filter[alqsm][condition][path]=field_alqsm.name&filter[alqsm][condition][value]=${value}&include=field_icone`;
 export const getCommanderieCroyantsData = (
-  langcode,
-  value = 'عناية أمير المومنين'
+    langcode,
+    value = 'عناية أمير المومنين',
 ) =>
-  `${api_url}/section?fields[node--section]=title,body,field_icone,field_lien,field_code_couleur&filter[langcode]=${langcode}&filter[alqsm][condition][path]=field_alqsm.name&filter[alqsm][condition][value]=${value}&include=field_icone`
+    `${api_url}/section?fields[node--section]=title,body,field_icone,field_lien,field_code_couleur&filter[langcode]=${langcode}&filter[alqsm][condition][path]=field_alqsm.name&filter[alqsm][condition][value]=${value}&include=field_icone`;
 export const getMawdouaData = (langcode, value = 'الاحاديت الموضوعة') =>
-  `${api_url}/mawdoua?fields[node--mawdoua]=title,body,field_icone,field_lien&filter[langcode]=${langcode}&filter[alkism][condition][path]=field_alkism.name&filter[alkism][condition][value]=${value}`
-export const getSlideHome = (langcode) =>
-  `${api_url}/slider?fields[node--slider]=field_liens,field_texte&filter[langcode]=${langcode}&include=field_images`
+    `${api_url}/mawdoua?fields[node--mawdoua]=title,body,field_icone,field_lien&filter[langcode]=${langcode}&filter[alkism][condition][path]=field_alkism.name&filter[alkism][condition][value]=${value}`;
+export const getSlideHome = langcode =>
+    `${api_url}/slider?fields[node--slider]=field_liens,field_texte&filter[langcode]=${langcode}&include=field_images`;
 export const AllVideo = () =>
-  `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video`
-
-export const getVideo = (title) =>
-  `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=${title}`
+    `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video`;
+export const getArticlesByTag = (value) => `${base_url}/meta-tags?field_tags_target_id=${value}`
+export const getVideo = title =>
+    `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=${title}`;
 // export const getArticleById = (title) => `${api_url}/article?include=field_image&fields[node--article]=title,body,created,field_image&filter[title]=${title}`
-export const getArticleById = (title) =>
-  `${api_url}/article?fields[node--article]=title,body,created,field_image&filter[alqsm][condition][path]=field_alqsm.name&filter[alqsm][condition][value]=${title}&include=field_tags.vid,field_image`
+// export const getArticleById = title =>
+//   `${api_url}/article?fields[node--article]=title,body&filter[alqsm][condition][path]=field_alqsm.name&filter[alqsm][condition][value]=${title}&include=field_tags.vid`;
+export const getArticleById = title =>
+    `${api_url}/article?fields[node--article]=title,body,created,field_image&filter[alqsm][condition][path]=field_alqsm.name&filter[alqsm][condition][value]=${title}&include=field_tags.vid,field_image`;
 export const getOurPartners = (langcode, value = 'شركاؤنا') =>
-  `${api_url}/section?include=field_icone&fields[node--section]=title,body,field_icone,field_lien&filter[langcode]=${langcode}&filter[alqsm][condition][path]=field_alqsm.name&filter[alqsm][condition][value]=${value}`
+    `${api_url}/section?include=field_icone&fields[node--section]=title,body,field_icone,field_lien&filter[langcode]=${langcode}&filter[alqsm][condition][path]=field_alqsm.name&filter[alqsm][condition][value]=${value}`;
 export const getSideArticle = (name, tid, parent_target) =>
-  `${base_url}/en/article/preview?name_1=${name}&tid=${tid}&parent_target_id=${parent_target}`
-export const getMenu = () => `${base_url}/en/menus`
-export const getMenuByName = (name) =>
-  `${base_url}/en/menus?name=${name}&parent_target_id=""`
-export const getMenuLinks = (show = 1) => `${base_url}/en/menus?show=${show}`
-export const getArticleByIdd = (title) =>
-  `${api_url}/article?include=field_image&fields[node--article]=title,body,field_image,field_titre&filter[field_titre]=${title}`
-export const getDegree = () => `${api_search}/api/degree`
-export const getSource = () => `${api_search}/api/source`
-export const getNarrator = () => `${api_search}/api/narrator`
-export const getTopic = () => `${api_search}/api/topic`
-export const getCategory = () => `${api_search}/api/categorie`
-export const Search = () => `${api_search}/api/search`
+    `${base_url}/en/article/preview?name_1=${name}&tid=${tid}&parent_target_id=${parent_target}`;
+export const getMenu = () => `${base_url}/en/menus`;
+export const getMenuByName = name =>
+    `${base_url}/en/menus?name=${name}&parent_target_id=""`;
+export const getMenuLinks = (show = 1) => `${base_url}/en/menus?show=${show}`;
+export const getArticleByIdd = title =>
+    `${api_url}/article?include=field_image&fields[node--article]=title,body,field_image,field_titre&filter[field_titre]=${title}`;
+export const getDegree = () => `${api_search}/api/degree`;
+export const getSource = () => `${api_search}/api/source`;
+export const getNarrator = () => `${api_search}/api/narrator`;
+export const getTopic = () => `${api_search}/api/topic`;
+export const getCategory = () => `${api_search}/api/categorie`;
+export const Search = () => `${api_search}/api/search`;
 // export const AllQuestions=()=>`${api_url}/question?fields[node--question]=title,field_description,created,drupal_internal__nid`
-export const getQuestionbyID = (nID) =>
-  `${base_url}/en/commentaire?entity_id=${nID}`
-export const SubmitQuestionAPI = () => `${api_url}/question`
-export const getQuestionAPI = () => `${api_url}/forum`
+export const getQuestionbyID = nID =>
+    `${base_url}/en/commentaire?entity_id=${nID}`;
+export const SubmitQuestionAPI = () => `${api_url}/question`;
+export const getQuestionAPI = () => `${api_url}/forum`;
 export const AllForums = () =>
-  `${api_url}/forum?fields[node--forum]=title,created,body,comment_forum,taxonomy_forums,drupal_internal__nid`
-export const getTaxonomyForum = () => `${base_url}/en/taxonomy/forums`
-export const getSideItems = (id) =>
-  `${base_url}/en/menu_by_parent?parent_target_id=${id}`
-export const newsletter = () => `${api_newsletter}`
-export const getLive = () => `${base_url}/live`
+    `${api_url}/forum?fields[node--forum]=title,created,body,comment_forum,taxonomy_forums,drupal_internal__nid`;
+export const getTaxonomyForum = () => `${base_url}/en/taxonomy/forums`;
+export const getSideItems = id =>
+    `${base_url}/en/menu_by_parent?parent_target_id=${id}`;
+export const newsletter = () => `${api_newsletter}`;
+export const getLive = () => `${base_url}/live`;
 export const getCarousel = () =>
-  `${api_url}/article?fields[node--article]=title,body&include=field_image&filter[field_est_slider]=1`
-export const getSlider = () => `${base_url}/en/slider`
-export const getNewSections = () => `${base_url}/section-accueil`
-export const getDataNewSections = (NewSection) =>
-  `${api_url}/section_accueil?include=field_icone_accueil&fields[node--section_accueil]=title,body,field_lien_accueil,field_code_couleur_accueil&filter[alqsm][condition][path]=field_alqsm_accueil.name&filter[alqsm][condition][value]=${NewSection}`
-export const addQuestions = () => `${api_search}/api/addQuestion`
-export const searchQuestion = () => `${api_search}/api/question/search`
+    `${api_url}/article?fields[node--article]=title,body&include=field_image&filter[field_est_slider]=1`;
+export const getSlider = () => `${base_url}/en/slider`;
+export const getNewSections = () => `${base_url}/section-accueil`;
+export const getDataNewSections = NewSection =>
+    `${api_url}/section_accueil?include=field_icone_accueil&fields[node--section_accueil]=title,body,field_lien_accueil,field_code_couleur_accueil&filter[alqsm][condition][path]=field_alqsm_accueil.name&filter[alqsm][condition][value]=${NewSection}`;
+export const addQuestions = () => `${api_search}/api/addQuestion`;
+export const searchQuestion = () => `${api_search}/api/question/search`;
 export const getVideo1 = () =>
-  `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=برامج على الشبكات الاجتماعية`
+    `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=برامج على الشبكات الاجتماعية`;
 export const getVideoBt = () =>
-  `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=برامج تلفزية`
+    `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=برامج تلفزية`;
 export const getVideoBi = () =>
-  `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=برامج اذاعية`
+    `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=برامج اذاعية`;
 export const getVideoDh = () =>
-  `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=الدروس الحديثية`
+    `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=الدروس الحديثية`;
 export const getVideoKi = () =>
-  `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=الدروس الحسنية`
+    `${api_url}/video?include=field_thumbnail_video,field_upload_video&fields[node--video]=title,field_description_video,field_lien_video,field_thumbnail_video,field_categorie_video,field_upload_video&&filter[field_categorie_video][condition][path]=field_categorie_video.name&filter[field_categorie_video][condition][value]=الدروس الحسنية`;
---
+++ pages/articlesByTag/[Id].js
@@ -0,0 +1,104 @@
+import React, {useEffect, useState} from 'react';
+import Link from 'next/link';
+import {Logos} from '../../assets';
+import styles from './articlesByTag.module.css';
+import _ from 'lodash';
+import {
+    base_url,
+    getArticlesByTag
+} from '../../endpoints';
+import FetchAPI from '../../API';
+import {useRouter} from "next/router";
+import Loading from "../../components/_UI/Loading";
+import TemplateArticle from "../../components/TemplateArticle";
+import Body from "../../components/Body";
+import ScrollButton from "../../components/ScrollButton";
+import Image from "next/image";
+
+const ArticlesByTag = props => {
+    const Id = useRouter()?.query?.title;
+    const [dataAPI, setDataAPI] = useState({});
+    console.log("--------55---", Id)
+
+    const url = getArticlesByTag(Id);
+    const getData = async () => {
+        FetchAPI(url).then(data => {
+            if (data.success) {
+                setDataAPI(data?.data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    if (_.isEmpty(dataAPI)) {
+        return (
+            <div className="d-flex align-items-center justify-content-center py-5">
+                <Loading/>
+            </div>
+        );
+    }
+
+    const myLoader = ({src, width, quality}) => {
+        return `${base_url}/${src}`
+    }
+    return (
+        <TemplateArticle {...props}
+                         titlePage="مقالات ذات صلة">
+            <Body className="TemplateArticleBody d-flex p-4">
+                <ScrollButton/>
+                <div className="flex-fill my-5">
+                    <div className='row tags'>
+                        {dataAPI && dataAPI?.map((data, index) => {
+                            return (
+                                <div key={index} className="card col col-12 col-lg-4 col-md-6 col-sm-12 m-2">
+                                    {data.field_image ?
+                                        <Image className="card-img-top px-1"
+                                               width={150}
+                                               height={150}
+                                               loader={myLoader}
+                                               src={data.field_image}
+                                               alt="Card image cap"/>
+                                        : <Image
+                                            className="card-img-top logoempty p-2"
+                                            src={Logos.logo_web}
+                                            width={150}
+                                            height={170}
+                                            alt="logo-Al-hadith-Mohammed-VI"
+                                            title="logo Al hadith Mohammed VI"/>
+                                    }
+                                    <div className="card-body">
+                                        <h5 className="card-title text-center">{data.title}</h5>
+                                        <p className="card-text">{data.body_1}</p>
+                                        <Link
+                                            passHref
+
+                                            // to={field_lien[0]?.uri.slice(9)}
+                                            href={{
+                                                pathname: `/article/${data.title}`,
+                                                search: '',
+                                                hash: '',
+                                                query: {
+                                                    title: data.title,
+                                                },
+                                            }}>
+                                            <a className="btn"
+                                               role="button">
+                                            <p style={{textAlign: 'center'}} className="m-0">{'إقرأ المزيد '}</p>
+                                            </a>
+                                            </Link>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </Body>
+            <ScrollButton/>
+        </TemplateArticle>
+    );
+};
+
+export default ArticlesByTag;
