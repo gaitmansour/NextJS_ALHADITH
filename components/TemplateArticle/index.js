4081f769ac6338eadba7fb8d83617de2cdc1d7a6
ghizlane <g.aitmansour@dialy.net>
2022-01-21 10:11:26 +0100

    Responsive

--- components/TemplateArticle/index.js
+++ components/TemplateArticle/index.js
@@ -5,45 +5,44 @@ import Footer from '../Footer'
 import Breadcrumb from '../_UI/Breadcrumb'
 import PageTitle from '../_UI/PageTitle'
 import styles from './TemplateArticle.module.css'
-import {Backgrounds} from '../../assets'
+import { Backgrounds } from '../../assets'
 import Image from 'next/image'
 import React from 'react'
 
 const TemplateArticle = (props) => {
-    return (
-        <Layout className={`${styles.TemplateArticle}TemplateArticle`}>
-            <TopBar {...props} />
-            <NavBar/>
-            <div style={{marginRight: 80}}>
-                <div
-                    className={`${styles.templateHeader} template-header position-relative pb-2`}
-                >
-                    <div
-                        className={` ${styles.bgDashed} p-1 flex-row-reverse position-absolute flex-row`}
-                        style={{left: 10, marginTop: 10}}
-                    >
-                        <Image
-                            alt={''}
-                            src={Backgrounds.bg_dashed}
-                            width={100}
-                            height={100}
-                            className={` bg_dashed h-100 position-absolute w-100`}
-                        />
-                    </div>
-                    <Breadcrumb data={props?.ListBreadcrumb}/>
-                    <PageTitle
-                        className={`${styles.pb0} pb-0 titre`}
-                        title={props?.titlePage}
-                        dateArticle={props?.dateArticlePage}
-                        created={props?.createdArticle}
-                    />
-
-                </div>
-                {props.children}
-            </div>
-            <Footer/>
-        </Layout>
-    )
+  return (
+    <Layout className={`${styles.TemplateArticle}TemplateArticle`}>
+      <TopBar {...props} />
+      <NavBar />
+      <div className={styles.bodyTemplate}>
+        <div
+          className={`${styles.templateHeader} template-header position-relative pb-2`}
+        >
+          <div
+            className={` ${styles.bgDashed} p-1 flex-row-reverse position-absolute flex-row`}
+            style={{ left: 10, marginTop: 10 }}
+          >
+            <Image
+              alt={''}
+              src={Backgrounds.bg_dashed}
+              width={100}
+              height={100}
+              className={` bg_dashed h-100 position-absolute w-100`}
+            />
+          </div>
+          <Breadcrumb data={props?.ListBreadcrumb} />
+          <PageTitle
+            className={`${styles.pb0} pb-0 titre`}
+            title={props?.titlePage}
+            dateArticle={props?.dateArticlePage}
+            created={props?.createdArticle}
+          />
+        </div>
+        {props.children}
+      </div>
+      <Footer />
+    </Layout>
+  )
 }
 
 export default TemplateArticle
