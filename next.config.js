adde16efabdcc7cdb4642168fa25005f4cf26f64
Dialy User <dialy-user@omnidata.ma>
2022-01-21 13:16:01 +0100

    Add CI/CD jobs

--- next.config.js
+++ next.config.js
@@ -2,27 +2,23 @@ module.exports = {
   experimental: {
     outputStandalone: true,
   },
+}
 
+module.exports = {
   eslint: {
-        // Warning: This allows production builds to successfully complete even if
+    // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
     },
 }
+
+
 const nextTranslate = require('next-translate')
 
-/*module.exports = nextTranslate()
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS({
-    cssLoaderOptions: {
-        url: false
-    }
-});*/
 const eslint={
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
 };
+
 const withPlugins = require("next-compose-plugins");
 
 module.exports = withPlugins([
