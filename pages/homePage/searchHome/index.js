04fbc0f099f7ef938a8240f2914a8b14cdcaa7a0
ghizlane <g.aitmansour@dialy.net>
2022-01-25 09:36:35 +0100

    Responsive

--- pages/homePage/searchHome/index.js
+++ pages/homePage/searchHome/index.js
@@ -16,7 +16,7 @@ import CustomModal from "../../../components/_UI/Modal";
 
 const SearchSection = (props) => {
     //const {state} = useLocation();
-    let router=useRouter();
+    let router = useRouter();
     const {state} = useRouter().query;
     const [showForm, setShowForm] = useState(true)
     const [dataDegree, setdataDegree] = useState([])
@@ -138,22 +138,12 @@ const SearchSection = (props) => {
                     topic: ChoiceCategory.label,
                     content: ChoiceDegree.label,
                     source: ChoiceSource.label,
-                    sourceHokm:EvaluationSource,
+                    sourceHokm: EvaluationSource,
                     narrator: ChoiceNarrator.label,
                     word: input
                 }
             })
         }
-console.log("search home")
-        console.log({
-            from: 'section',
-            topic: ChoiceCategory.label,
-            door: ChoiceTopic,
-            content: ChoiceDegree.label,
-            source: ChoiceSource,
-            narrator: ChoiceNarrator,
-            word: input
-        })
     }
     const handleKeyDown = (event, location) => {
         // console.log('A key was pressed', event.keyCode);
@@ -187,94 +177,94 @@ console.log("search home")
         getDataCategory()
     }, [])
 
-  return (
-    <div className={`${styles.secSearch} container flex-fill secSearch`}>
-        <div className={`container ${styles.SearchSection} `}>
-      <SectionTitle title='البحث' className='' />
-        </div>
-      <div
-        ref={resultsRef}
-        className={`${styles.searchElement} d-flex flex-row align-items-center justify-content-between mt-4`}
-      >
-        <SearchInput
-          styleSerachIcon={{ backgroundColor: '#656e7e' }}
-          styleIcon={{ color: '#656e7e', width: 20 }}
-          styleDiv={{position:'absolute',right:"78%"}}
-          styleFilter={{ backgroundColor: '#656e7e', width: 50 }}
-          onClickSettings={() => setShowForm(!showForm)}
-          input={input}
-          onChange={(v) => handleInput(v)}
-          placeholder='البحث في منصة محمد السادس للحديث النبوي الشريف'
-          className='bg-white mx-0'
-          clickSearch={() => handleClickSearch()}
-        />
-
-        <div
-          className='box-icon-setting d-flex align-items-center align-self-center btn mx-2 mb-2 p-0'
-          onClick={() => handleClickSearch()}
-        >
-          <i
-            className='fas fa-search p-3 text-light'
-            style={{ backgroundColor: '#157646', borderRadius: 8 }}
-          />
-        </div>
-      </div>
-      {showForm && (
-        <Cards
-          className={`${styles.formSearch} form-search p-2`}
-          style={{ backgroundColor: '#e9fbf1' }}
-        >
-          <div className='d-flex flex-wrap flex-column'>
-            <div className={`${styles.alignsec} d-flex alignIte`}>
-              {/*<Input className="col-md-4" label="نص الحديث" placeholder="ابحث نص الحديث"/>
-               */}
-              <CustomSelect
-                className='col-md-4'
-                options={dataCategory && dataCategory}
-                defaultInputValue={ChoiceCategory ? ChoiceCategory : ''}
-                label='موضوع'
-                placeholder='اكتب الموضوع'
-                onChange={(v) => setChoiceCategory(v)}
-              />
-
-              <Input
-                className='col-md-4'
-                label='مصدر الحكم'
-                placeholder='ابحث بمصدر الحكم'
-                value={EvaluationSource && EvaluationSource}
-                onChange={(v) => setEvaluationSource(v.target.value)}
-              />
+    return (
+        <div className={`${styles.secSearch} container flex-fill secSearch`}>
+            <div className={`container ${styles.SearchSection} `}>
+                <SectionTitle title='البحث' className=''/>
             </div>
-            <div className={`${styles.alignsec} d-flex alignIte`}>
-              <CustomSelect
-                className='col-md-4'
-                options={dataSource && dataSource}
-                defaultInputValue={ChoiceSource ? ChoiceSource : ''}
-                label='المصدر'
-                placeholder='اكتب اسم المصدر'
-                onChange={(v) => {
-                  setChoiceSource(v)
-                }}
-              />
+            <div
+                ref={resultsRef}
+                className={`${styles.searchElement} d-flex flex-row align-items-center justify-content-between mt-4`}
+            >
+                <SearchInput
+                    styleSerachIcon={{backgroundColor: '#656e7e'}}
+                    styleIcon={{color: '#656e7e', width: 20}}
+                    styleDiv={{position: 'absolute', right: "78%"}}
+                    styleFilter={{backgroundColor: '#656e7e', width: 50}}
+                    onClickSettings={() => setShowForm(!showForm)}
+                    input={input}
+                    onChange={(v) => handleInput(v)}
+                    placeholder='البحث في منصة محمد السادس للحديث النبوي الشريف'
+                    className='bg-white mx-0'
+                    clickSearch={() => handleClickSearch()}
+                />
 
-              <CustomSelect
-                className='col-md-4'
-                options={dataDegree && dataDegree}
-                label='الحكم'
-                defaultInputValue={ChoiceDegree ? ChoiceDegree : ''}
-                placeholder='اكتب الحكم'
-                onChange={(v) => setChoiceDegree(v)}
-              />
-
-                        <CustomSelect className="col-md-4"
-                                      options={dataNarrator && dataNarrator}
-                                      label="الراوي"
-                                      defaultInputValue={ChoiceNarrator ? ChoiceNarrator : ""}
-                                      placeholder="اكتب اسم الراوي"
-                                      onChange={v => setChoiceNarrator(v)}/>
-                    </div>
+                <div
+                    className='box-icon-setting d-flex align-items-center align-self-center btn mx-2 mb-2 p-0'
+                    onClick={() => handleClickSearch()}
+                >
+                    <i
+                        className='fas fa-search p-3 text-light'
+                        style={{backgroundColor: '#157646', borderRadius: 8}}
+                    />
                 </div>
-            </Cards>)}
+            </div>
+            {showForm && (
+                <Cards
+                    className={`${styles.formSearch} form-search p-2`}
+                    style={{backgroundColor: '#e9fbf1'}}
+                >
+                    <div className='d-flex flex-wrap flex-column'>
+                        <div className={`${styles.alignsec} d-flex alignIte`}>
+                            {/*<Input className="col-md-4" label="نص الحديث" placeholder="ابحث نص الحديث"/>
+               */}
+                            <CustomSelect
+                                className='col-md-4'
+                                options={dataCategory && dataCategory}
+                                defaultInputValue={ChoiceCategory ? ChoiceCategory : ''}
+                                label='موضوع'
+                                placeholder='اكتب الموضوع'
+                                onChange={(v) => setChoiceCategory(v)}
+                            />
+
+                            <Input
+                                className='col-md-4'
+                                label='مصدر الحكم'
+                                placeholder='ابحث بمصدر الحكم'
+                                value={EvaluationSource && EvaluationSource}
+                                onChange={(v) => setEvaluationSource(v.target.value)}
+                            />
+                        </div>
+                        <div className={`${styles.alignsec} d-flex alignIte`}>
+                            <CustomSelect
+                                className='col-md-4'
+                                options={dataSource && dataSource}
+                                defaultInputValue={ChoiceSource ? ChoiceSource : ''}
+                                label='المصدر'
+                                placeholder='اكتب اسم المصدر'
+                                onChange={(v) => {
+                                    setChoiceSource(v)
+                                }}
+                            />
+
+                            <CustomSelect
+                                className='col-md-4'
+                                options={dataDegree && dataDegree}
+                                label='الحكم'
+                                defaultInputValue={ChoiceDegree ? ChoiceDegree : ''}
+                                placeholder='اكتب الحكم'
+                                onChange={(v) => setChoiceDegree(v)}
+                            />
+
+                            <CustomSelect className="col-md-4"
+                                          options={dataNarrator && dataNarrator}
+                                          label="الراوي"
+                                          defaultInputValue={ChoiceNarrator ? ChoiceNarrator : ""}
+                                          placeholder="اكتب اسم الراوي"
+                                          onChange={v => setChoiceNarrator(v)}/>
+                        </div>
+                    </div>
+                </Cards>)}
             <CustomModal title={'تنبيه'}
                          body={'يرجى ملء كلمة البحث '}
                          show={show}
