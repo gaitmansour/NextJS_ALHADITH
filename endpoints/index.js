dd3914bb660b1b94397f98eb876fbb04524e08dd
Dialy User <dialy-user@omnidata.ma>
2022-02-21 10:21:05 +0100

    publish to prod

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,10 +1,14 @@
 //export const base_url = `https://back.alhadith.dialtechnologies.net`
-export const base_url = `https://backend.7adith.ma:8000`
+//export const base_url = `https://backend.7adith.ma:8000`
+export const base_url = `https://backend.hadithm6.ma`
 const { BASE_URL_CMS } = process.env
 export const api_url = `${base_url}/en/jsonapi/node`
 //export const api_url = `https://backend.7adith.ma:8000/en/jsonapi/node`
-export const api_search = `https://apisearch.7adith.ma:8000`
-export const api_newsletter = `https://apisearch.7adith.ma:8000/api/addmembre`
+export const api_search = `https://apisearch.hadithm6.ma`
+export const api_newsletter = `https://apisearch.hadithm6.ma/api/addmembre`
+
+//export const api_search = `https://apisearch.7adith.ma:8000`
+//export const api_newsletter = `https://apisearch.7adith.ma:8000/api/addmembre`
 // export const base_url = `https://api.pray.zone/`
 
 export const getResourcesData = (langcode, value = 'موارد') =>
