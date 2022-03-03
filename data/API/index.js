35054152951fb85e65c39e01e7a85fe3ef2c6c32
m-benrhabra <meriem.benrhabra1@gmail.com>
2022-03-03 18:25:30 +0100

    Add Question

--- data/API/index.js
+++ data/API/index.js
@@ -1,47 +1,48 @@
-import axios from "axios";
+import axios from 'axios'
 import React from 'react'
 
 export const FetchAPIWthData = async (url, data) => {
-    return await axios.post(url, data, {
-        headers: {
-            "Content-Type": "application/vnd.api+json",
-            "Authorization": "Basic ZGlhbHk6ZGlhbHk="
-        }
-    }).then(function (response) {
-        // handle success
-        // console.log(response);
-        if (response.status === 201) {
-            const data = {
-                data: response?.data,
-                success: true
-            }
-            return data
-        }
-        if (response.status === 500) {
-            const data = {
-                data: null,
-                success:false,
-                status: response.status,
-                message: "bla bla bla"
-            }
-            return data
-        }
-        const data = {
-            data: null,
-            success: false,
-            message: response.data
-        }
-
-        return data
+  return await axios
+    .post(url, data, {
+      headers: {
+        // "Content-Type": "application/vnd.api+json",
+        // "Authorization": "Basic ZGlhbHk6ZGlhbHk="
+      },
     })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-            return {
-                data: null,
-                success: false,
-                message: ""
-            }
-        })
+    .then(function (response) {
+      // handle success
+      // console.log(response);
+      if (response.status === 201) {
+        const data = {
+          data: response?.data,
+          success: true,
+        }
+        return data
+      }
+      if (response.status === 500) {
+        const data = {
+          data: null,
+          success: false,
+          status: response.status,
+          message: 'bla bla bla',
+        }
+        return data
+      }
+      const data = {
+        data: null,
+        success: false,
+        message: response.data,
+      }
 
+      return data
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error)
+      return {
+        data: null,
+        success: false,
+        message: '',
+      }
+    })
 }
