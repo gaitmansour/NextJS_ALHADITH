2299fe9c68808d5433b603da10fdad4cc430974d
m-benrhabra <meriem.benrhabra1@gmail.com>
2022-03-14 13:07:43 +0100

    styles input search

--- components/Forms/CustomSelect/index.js
+++ components/Forms/CustomSelect/index.js
@@ -1,83 +1,96 @@
-import React from 'react';
+import React from 'react'
 
-import Select from 'react-select';
+import Select from 'react-select'
 import styles from './CustomSelect.module.css'
 
 const CustomSelect = (props) => {
+  const className = props?.className ? props.className : ''
+  const classNameSelect = props?.classNameSelect ? props.classNameSelect : ''
+  const {
+    message,
+    label,
+    placeholder,
+    name,
+    options,
+    onChange,
+    value,
+    defaultInputValue,
+  } = props
 
-    const className = props?.className ? props.className : ""
-    const classNameSelect = props?.classNameSelect ? props.classNameSelect : ""
-    const {message, label, placeholder, name, options, onChange, value, defaultInputValue} = props
+  const colourStyles = {
+    menuPortal: (base) => ({ ...base, zIndex: 9999, backgroundColor: 'red' }),
+    menu: (provided) => ({ ...provided, zIndex: 9999 }),
+    control: (styles) => ({ ...styles, backgroundColor: 'white' }),
+    option: (provided, state) => {
+      return {
+        ...provided,
+        backgroundColor: state.isSelected ? '#157646' : 'transparent',
+        color: state.isSelected ? '#fff' : '#000',
 
-    const colourStyles = {
-        menuPortal: base => ({...base, zIndex: 9999,backgroundColor:'red'}),
-        menu: provided => ({ ...provided, zIndex: 9999 }),
-        control: styles => ({...styles, backgroundColor: 'white'}),
-        option: (provided, state) => {
-            return ({
-                ...provided,
-                backgroundColor: state.isSelected ? "#157646" : "transparent",
-                color: state.isSelected ? "#fff" : "#000",
-
-                // backgroundColor: state.data === state.selectProps.value ? `#157646` : `transparent`,
-                textAlign: 'right',
-                '&:hover': {
-                    color: "#fff",
-                    background: "#15764685",
-                },
-            });
+        // backgroundColor: state.data === state.selectProps.value ? `#157646` : `transparent`,
+        textAlign: 'right',
+        '&:hover': {
+          color: '#fff',
+          background: '#15764685',
         },
-        multiValue: (styles, {data}) => {
-            return {
-                ...styles,
-                color: "#FFFFFF",
-                backgroundColor: "#109d58",
-            };
-        },
-        menuList: (styles, {data}) => ({
-            ...styles,
-            maxHeight: 140,
-        }),
-        multiValueLabel: (styles, {data}) => ({
-            ...styles,
-            color: data.color,
-        }),
-        multiValueRemove: (styles, {data}) => ({
-            ...styles,
-            color: data.color,
-            ':hover': {
-                backgroundColor: data.color,
-                color: 'white',
-            },
-        }),
-    };
+      }
+    },
+    multiValue: (styles, { data }) => {
+      return {
+        ...styles,
+        color: '#FFFFFF',
+        backgroundColor: '#109d58',
+      }
+    },
+    menuList: (styles, { data }) => ({
+      ...styles,
+      maxHeight: 140,
+    }),
+    multiValueLabel: (styles, { data }) => ({
+      ...styles,
+      color: data.color,
+    }),
+    multiValueRemove: (styles, { data }) => ({
+      ...styles,
+      color: data.color,
+      ':hover': {
+        backgroundColor: data.color,
+        color: 'white',
+      },
+    }),
+  }
 
-    return (
-        <div className={`custom-input flex-fill ${className}`}>
-            <div className="d-flex flex-column flex-fill mb-3 px-2">
-                {label && <label className="form-label">{label}</label>}
+  return (
+    <div className={`custom-input flex-fill ${className}`}>
+      <div className='d-flex flex-column flex-fill mb-3 px-2'>
+        {label && <label className='form-label'>{label}</label>}
 
-                <Select
-                    className={`${styles.customSelect} custom-select ${classNameSelect}`}
-                    placeholder={placeholder}
-                    isMulti={false}
-                    isClearable
-                    menuPortalTarget={typeof window !== "undefined" && document.body}
-                    value={defaultInputValue}
-                    name={"form-field-name"}
-                    options={options}
-                    onChange={data => onChange ? onChange(data) : {}}
-                    //menuPortalTarget={document.body}
-                    styles={colourStyles}
-                    // defaultInputValue={defaultInputValue}
-                    noOptionsMessage={() => 'لا يوجد خيارات'}
-                />
+        <Select
+          className={`${defaultInputValue ? styles.borderInputSelect : ''} ${
+            styles.customSelect
+          } custom-select ${classNameSelect}`}
+          placeholder={placeholder}
+          isMulti={false}
+          isClearable
+          menuPortalTarget={typeof window !== 'undefined' && document.body}
+          value={defaultInputValue}
+          name={'form-field-name'}
+          options={options}
+          onChange={(data) => (onChange ? onChange(data) : {})}
+          //menuPortalTarget={document.body}
+          styles={colourStyles}
+          // defaultInputValue={defaultInputValue}
+          noOptionsMessage={() => 'لا يوجد خيارات'}
+        />
 
-                {message && <div className="form-text">We'll never share your email with anyone else.</div>}
-            </div>
-        </div>
-    );
-
+        {message && (
+          <div className='form-text'>
+            We'll never share your email with anyone else.
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default CustomSelect
--- components/Forms/Input/index.js
+++ components/Forms/Input/index.js
@@ -1,19 +1,29 @@
-import styles from './Input.module.css';
+import styles from './Input.module.css'
 
 const Input = (props) => {
+  const className = props?.className ? props.className : ''
+  const { message, label } = props
 
-    const className = props?.className ? props.className : ""
-    const {message, label,} = props
-
-    return (
-        <div className={`${styles.customInput} custom-input flex-fill ${className}`}>
-            <div className="d-flex flex-column flex-fill mb-3 px-2">
-                {label && <label className={`${styles.formLabel} form-label`}>{label}</label>}
-                <input {...props} className="form-control"/>
-                {message && <div className="form-text">{"We'll never share your email with anyone else."}</div>}
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={`${styles.customInput} custom-input flex-fill ${className}`}
+    >
+      <div className='d-flex flex-column flex-fill mb-3 px-2'>
+        {label && (
+          <label className={`${styles.formLabel} form-label`}>{label}</label>
+        )}
+        <input
+          {...props}
+          className={`${props.value ? styles.borderInput : ''} form-control`}
+        />
+        {message && (
+          <div className='form-text'>
+            {"We'll never share your email with anyone else."}
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Input
