653e6004a06be2f7a13e01ba097c56635c70ad61
Amine <malaoui@dialy.net>
2022-04-04 23:57:09 +0000

    change logo with center text version

--- assets/Images/logos/index.js
+++ assets/Images/logos/index.js
@@ -1,7 +1,7 @@
 import our_partners from './OurPartners'
 
 const Logos = {
-  logo_web: require('./logo-alhadith-mohamed-6.svg'),
+  logo_web: require('./logo-alhadith-mohamed-6-center.svg'),
   logo_new: require('./logo_M6_hadith.svg'),
   logo_portail_hadith: require('./logo_portail_hadith_m6.svg'),
   our_partners,
