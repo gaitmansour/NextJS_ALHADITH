5cb75c445e6c2248c6d05cbcef11006e366fb1dc
Amine <malaoui@dialy.net>
2022-04-16 01:16:23 +0000

    add article condition User

--- pages/conditionUser/index.js
+++ pages/conditionUser/index.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./conditionUser.module.css"
 import TemplateArticle from "../../components/TemplateArticle";
 import Body from "../../components/Body";
+import {getArticleById} from "../../endpoints";
+import FetchAPI from "../../API";
+import _ from "lodash";
+import Loading from "../../components/_UI/Loading";
 
 const ConditionUser = (props) => {
+    const [dataAPI, setDataAPI] = useState([])
     const data = [
         {
             "title": "الرئيسية",
@@ -15,13 +20,38 @@ const ConditionUser = (props) => {
         }
     ]
 
+    const url = getArticleById('اتفاقية استخدام الموقع')
+    const getData = async () => {
+        FetchAPI(url).then((data) => {
+            if (data.success) {
+                setDataAPI(data?.data)
+            }
+        })
+        // console.log('data condition', dataAPI?.data[0]?.attributes?.body?.value)
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+
+    if (_.isEmpty(dataAPI)) {
+        return (
+            <div className='d-flex align-items-center justify-content-center py-5'>
+                <Loading />
+            </div>
+        )
+    }
 
     return (
         <TemplateArticle ListBreadcrumb={data} titlePage="اتفاقية استخدام الموقع">
             <Body className="TemplateArticleBody Media d-flex p-4">
-                <div className={'p-5'}>
-                    <p>لا توجد معلومات متوفرة الآن</p>
-                </div>
+                <>
+                    <div
+                        className={styles.desc}
+                        dangerouslySetInnerHTML={{
+                            __html: dataAPI?.data[0]?.attributes?.body?.value,
+                        }}
+                    />
+                </>
                </Body>
         </TemplateArticle>
     );
