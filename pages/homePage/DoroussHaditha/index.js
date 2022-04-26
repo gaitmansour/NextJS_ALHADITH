cba4899055fef7d3d6d0b07b69d5cb149f33e428
m-benrhabra <meriem.benrhabra1@gmail.com>
2022-04-26 10:02:59 +0000

    fix carousel 'الدروس الحديثية' home page

--- pages/homePage/DoroussHaditha/index.js
+++ pages/homePage/DoroussHaditha/index.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Tabs, Tab } from 'react-bootstrap'
 import { data } from '../../../data/DoroussData'
 import DoroussTab from './DoroussTab'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import styles from './DoroussHaditha.module.css'
 import SectionTitle from '../../../components/_UI/SectionTitle'
+import FetchAPI from '../../../API'
+import { getSideItems } from '../../../endpoints'
 
 const DoroussHaditha = () => {
-  const [key, setKey] = useState('التمهيدية')
+  const [key, setKey] = useState('الدروس التمهيدية')
+  const [categoryMedia, setCategoryMedia] = useState([])
+
+  const getItemsMenu = async () => {
+    return FetchAPI(getSideItems(51)).then((data) => {
+      if (data.success) {
+        setCategoryMedia(data?.data)
+      }
+    })
+  }
+
+  useEffect(() => {
+    getItemsMenu()
+  }, [])
+
+  console.log('categoryMedia ==>', categoryMedia)
 
   return (
     <div className='mt-5'>
@@ -23,18 +40,18 @@ const DoroussHaditha = () => {
         variant={'tabs'}
         activeKey={key}
         className={`${
-          key === 'البيانية'
+          key === 'الدروس البيانية'
             ? 'navTabs2'
-            : key === 'التمهيدية'
+            : key === 'الدروس التمهيدية'
             ? 'navTabs'
             : 'navTabs1'
         }
       text-secondary  mt-5`}
         style={{ justifyContent: 'center' }}
       >
-        {data.map((t, key) => (
-          <Tab key={key} {...t}>
-            <DoroussTab tid={t.tid} title={t.title} />
+        {categoryMedia.map((t, key) => (
+          <Tab key={key} eventKey={t.name} title={t.name}>
+            <DoroussTab tid={t.tid} title={t.name} />
           </Tab>
         ))}
       </Tabs>
